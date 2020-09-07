b15ffe1ab4040447e31f6c6b4a30fd7aac13c34f
Vika <viktoriamaksi91@gmail.com>
2020-09-07 17:29:11 +0300

    Create User account

---
+++ src/Controller/UserController.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+/**
+ *
+ * Class UserController
+ * @package App\Controller
+ */
+class UserController extends AbstractController
+{
+    /**
+     * @param AuthenticationUtils $utils
+     * @return /Response
+     * @Route("/profile", name="profile_info")
+     */
+    public function index(AuthenticationUtils $utils)
+    {
+        $error = $utils->getLastAuthenticationError();
+
+        $last_username = $utils->getLastUsername();
+
+        return $this->render('user/index.html.twig',[
+            'last_username' => $last_username,
+            'error' => $error
+        ]);
+    }
+}
