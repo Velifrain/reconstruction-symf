44a5fe86352b3250395d50c53950a4bc14be17da
Vika <viktoriamaksi91@gmail.com>
2020-09-18 17:28:02 +0300

    Files upload inventory

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -8,6 +8,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
+
 /**
  *
  * Class UserController
